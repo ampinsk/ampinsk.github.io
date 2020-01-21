c4dc01b4af3d18ac78ed274054c1ee1a4bb1beb9
Amanda Pinsker <ampinsk@github.com>
2020-01-21 13:56:27 -0800

    playing

--- js/main.js
+++ js/main.js
@@ -4,3 +4,50 @@ window.sr = ScrollReveal({
   scale: .95
 });
 
+var link1 = document.getElementById('link1');
+var link2 = document.getElementById('link2');
+var link3 = document.getElementById('link3');
+var link4 = document.getElementById('link4');
+var link5 = document.getElementById('link5');
+
+var body = document.body;
+
+link1.onmouseover = function() {
+	body.className = 'hovered1';
+}
+
+link1.onmouseout = function() {
+	body.className = '';
+}
+
+link2.onmouseover = function() {
+	body.className = 'hovered2';
+}
+
+link2.onmouseout = function() {
+	body.className = '';
+}
+
+link3.onmouseover = function() {
+	body.className = 'hovered3';
+}
+
+link3.onmouseout = function() {
+	body.className = '';
+}
+
+link4.onmouseover = function() {
+	body.className = 'hovered4';
+}
+
+link4.onmouseout = function() {
+	body.className = '';
+}
+
+link5.onmouseover = function() {
+	body.className = 'hovered5';
+}
+
+link5.onmouseout = function() {
+	body.className = '';
+}
