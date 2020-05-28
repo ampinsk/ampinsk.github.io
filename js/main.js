07d404afd20efa96ae186895db781db15e3aeaa7
Amanda Pinsker <ampinsk@github.com>
2020-05-28 12:36:48 -0700

    Make my js less bad

--- js/main.js
+++ js/main.js
@@ -1,100 +1,43 @@
+// Scrolling behavior
+
 window.sr = ScrollReveal({
   duration: 800,
   distance: '10px',
   scale: .95
 });
 
+// Hover states
+
 var body = document.body;
-var external = document.getElementsByClassName("external-link");
-var external2 = document.getElementById('external2');
 
-
-kleo.onmouseover = function() {
-	body.className = 'kleo';
+function addClass(element, nameOfClass) {
+	if (element.classList) {
+ 		element.classList.add(nameOfClass);
+	}
+	else {
+		var regExp = new RegExp('(\\s|^)' + nameOfClass + '(\\s|$)');
+		if (!element.className.match(regExp)) {
+			element.className += " " + nameOfClass;
+		}
+	}
 }
 
-kleo.onmouseout = function() {
-	body.className = '';
-  console.log("my javascript is very bad don't judge me");
+function removeClass(element, nameOfClass) {
+	if (element.classList) {
+		element.classList.remove(nameOfClass);
+	}
+	else {
+		var regExp = new RegExp('(\\s|^)' + nameOfClass + '(\\s|$)');
+		if (element.className.match(regExp)) {
+			element.className.replace(regExp, ' ');
+		}
+	}
 }
 
-desktop.onmouseover = function() {
-	body.className = 'desktop';
-}
+// Easter egg
 
-desktop.onmouseout = function() {
-	body.className = '';
-}
+// I super got this from stack overflow
 
-baudelaire.onmouseover = function() {
-	body.className = 'baudelaire';
-}
-
-baudelaire.onmouseout = function() {
-	body.className = '';
-}
-
-read.onmouseover = function() {
-	body.className = 'read';
-}
-
-read.onmouseout = function() {
-	body.className = '';
-}
-
-cli.onmouseover = function() {
-	body.className = 'cli';
-}
-
-cli.onmouseout = function() {
-	body.className = '';
-}
-
-asterisk.onmouseover = function() {
-  asterisk2.className= 'footnote-accent note';
-}
-
-asterisk.onmouseout = function() {
-  asterisk2.className= 'note';
-}
-
-dagger.onmouseover = function() {
-  dagger2.className= 'footnote-accent note';
-}
-
-dagger.onmouseout = function() {
-  dagger2.className= 'note';
-}
-
-doubledagger.onmouseover = function() {
-  doubledagger2.className= 'footnote-accent note';
-}
-
-doubledagger.onmouseout = function() {
-  doubledagger2.className= 'note';
-}
-
-section.onmouseover = function() {
-  section2.className= 'footnote-accent note';
-  console.log("hi!!!!!!");
-}
-
-section.onmouseout = function() {
-  section2.className= 'note';
-}
-
-paragraph.onmouseover = function() {
-  paragraph2.className= 'footnote-accent note';
-  console.log("my javascript is very bad don't judge me");
-}
-
-paragraph.onmouseout = function() {
-  paragraph2.className= 'note';
-}
-
-
-
-// a key map of allowed keys
 var allowedKeys = {
   65: 'a',
   77: 'm',
@@ -102,38 +45,34 @@ var allowedKeys = {
   68: 'd',
 };
 
-// the 'official' Konami Code sequence
-var konamiCode = ['a', 'm', 'a', 'n', 'd', 'a'];
+var amanamiCode = ['a', 'm', 'a', 'n', 'd', 'a'];
 
 // a variable to remember the 'position' the user has reached so far.
-var konamiCodePosition = 0;
+var amanamiCodePosition = 0;
 
 // add keydown event listener
 document.addEventListener('keydown', function(e) {
   // get the value of the key code from the key map
   var key = allowedKeys[e.keyCode];
-  // get the value of the required key from the konami code
-  var requiredKey = konamiCode[konamiCodePosition];
+  // get the value of the required key from the amanami code
+  var requiredKey = amanamiCode[amanamiCodePosition];
 
   // compare the key with the required key
   if (key == requiredKey) {
 
-    // move to the next key in the konami code sequence
-    konamiCodePosition++;
+    // move to the next key in the amanami code sequence
+    amanamiCodePosition++;
 
     // if the last key is reached, activate cheats
-    if (konamiCodePosition == konamiCode.length) {
+    if (amanamiCodePosition == amanamiCode.length) {
       activateCheats();
-      konamiCodePosition = 0;
+      amanamiCodePosition = 0;
     }
   } else {
-    konamiCodePosition = 0;
+    amanamiCodePosition = 0;
   }
 });
 
 function activateCheats() {
-  body.className = "easter-egg";
-}
-function resetCheats() {
-  body.className = "";
+  body.classList.add("easter-egg");
 }
